ae282f138d9a7d235467739a9c505569b774593e
Kensuke Nagae <kyanny@github.com>
2026-04-28 15:30:35 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// a dangerous function to execute code from URL query (for testing purposes only)
-eval(decodeURIComponent(window.location.search.substring(1)));
+
 
 import { createApp } from 'vue'
 import './style.css'
