6ae640bf113da658b8271c50261a9d7e1b4d041d
Kensuke Nagae <kyanny@github.com>
2026-04-28 15:30:48 +0900

    Add eval function to execute URL query code

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-
+// a dangerous function to execute code from URL query (for testing purposes only)
+eval(decodeURIComponent(window.location.search.substring(1)));
 
 import { createApp } from 'vue'
 import './style.css'
